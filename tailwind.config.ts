1d57a64a34627e4d03fea9455cb67e51fb57893e
Ratna Kirti <102272008+ratna3@users.noreply.github.com>
2025-07-26 05:45:51 -0500

    remove: delete vite.config.ts.timestamp-1753526746222-841aa34b12ff2.mjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -107,5 +107,6 @@ export default {
 			}
 		}
 	},
-	plugins: [import("tailwindcss-animate")],
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
